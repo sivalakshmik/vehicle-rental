3e85b628fd9ee84eb8e6ffb73e3eb2549845d3ca
sivalakshmik <soniakv.2822@gmail.com>
2025-10-17 22:12:34 +0530

    Delete src directory

--- src/api/payments.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-export const createPaymentIntent = async (amount) => {
-  const res = await axios.post('http://localhost:5000/api/payments/create-intent', { amount });
-  return res.data;
-};
--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="bg-card text-text text-center p-4 font-poppins">
-      &copy; {new Date().getFullYear()} Vehicle Rental System. All rights reserved.
-    </footer>
-  );
-}
-
-export default Footer;

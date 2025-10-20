36e6cdf3111cc9ceb2c72e28f1000d163bd58bce
sivalakshmik <soniakv.2822@gmail.com>
2025-10-20 19:53:10 +0530

    Update vehicles.js

--- src/api/vehicles.js
+++ src/api/vehicles.js
@@ -1,13 +1,15 @@
 import axios from 'axios';
 // 
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 export const getVehicles = async () => {
-  const res = await axios.get('http://localhost:5000/api/vehicles');
+  const res = await axios.get('${API_BASE_URL}/api/vehicles');
   return res.data;
 };
 // 
 export const getVehicleById = async (id) => {
-  const res = await axios.get(`http://localhost:5000/api/vehicles/${id}`);
+  const res = await axios.get(`${API_BASE_URL}/api/vehicles/${id}`);
   return res.data;
 };
+
 
 

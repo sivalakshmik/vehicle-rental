1097332d9bad3edd87cc45eb142661b01be273bf
sivalakshmik <soniakv.2822@gmail.com>
2025-10-20 19:58:20 +0530

    Fix formatting in payments.js

--- src/api/payments.js
+++ src/api/payments.js
@@ -1,7 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
+
 const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+
 export const createPaymentIntent = async (amount) => {
-  const res = await axios.post('${API_BASE_URL}/api/payments/create-intent', { amount });
+
+  const res = await axios.post(`${API_BASE_URL}/api/payments/create-intent`, { amount });
   return res.data;
 };
-

32cc6f7e7a366111f344fe37c1e28e0503c351bc
sivalakshmik <soniakv.2822@gmail.com>
2025-10-20 19:52:17 +0530

    Update payments.js

--- src/api/payments.js
+++ src/api/payments.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
-
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 export const createPaymentIntent = async (amount) => {
-  const res = await axios.post('http://localhost:5000/api/payments/create-intent', { amount });
+  const res = await axios.post('${API_BASE_URL}/api/payments/create-intent', { amount });
   return res.data;
 };
+
